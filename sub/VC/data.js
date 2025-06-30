41c6e8bebdb122801b36a352928903b74e4d243a
Addison Thompson <addi.thompson13@gmail.com>
2025-06-30 11:31:43 -0400

    Added the Vibe check (VC) walkthrough, and finished the configurations as well

---
+++ sub/VC/data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama3",
+      "name": "Panorama(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.007184001413516938,
+          "pitch": 0.03887086279533669,
+          "rotation": 0,
+          "target": "1-scene-91"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.46598861236452827,
+          "pitch": 0.19275419998891152,
+          "title": "Good question",
+          "text": "More questions?"
+        }
+      ],
+      "relaxHotspots": [
+        {
+          "yaw": -0.46598861236452827,
+          "pitch": 0.03887086279533669,
+          "title": "Good question",
+          "text": "More questions?"
+        }
+      ]
+    },
+    {
+      "id": "1-scene-91",
+      "name": "Scene 9(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5894500913730436,
+        "pitch": 0.18470382398880147,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.829626262649466,
+          "pitch": 0.060219344963856614,
+          "rotation": 0,
+          "target": "0-panorama3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9680695419168366,
+          "pitch": -0.05965844289494626,
+          "title": "This is a Test",
+          "text": "Here is your text: <a href='https://placehold.co/' target='_blank'>https://placehold.co/</a>"
+        }
+      ],
+      "buildHotspots": [
+        {
+          "yaw": 1.829626262649466,
+          "pitch": -0.05965844289494626,
+          "title": "This is a Test",
+          "text": "Here is your text: <a href='https://placehold.co/' target='_blank'>https://placehold.co/</a>"
+        }
+      ]
+    }
+  ],
+  "name": "EJH_Test-R01",
+  "walkthrough": "VC",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
